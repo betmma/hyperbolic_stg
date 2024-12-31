68270fcbcf5e01f3e89949fd5ad76a0c7da9f0c0
betmma <116655315+betmma@users.noreply.github.com>
2024-12-31 21:29:13 +0800

    optimize the main menu pattern

--- backgroundPattern.lua
+++ backgroundPattern.lua
@@ -19,8 +19,8 @@ BackgroundPattern.Empty=Empty
 -- sideNum=5 angleNum=4 -> r=107
 -- sideNum=4 angleNum=5 -> r=126.2
 -- sideNum=3 angleNum=7 -> r=110
--- point: where pattern begins. angle: direction of first line. sideNum: useless now as I dunno how to calculate side length. angleNum: how many sides are connected to each point. iteCount: used for recursion. plz input 0. r: side length. drawedPoints: plz input {}. color: {r,g,b}
-local function tesselation(point,angle,sideNum,angleNum,iteCount,r,drawedPoints,color)
+-- point: where pattern begins. angle: direction of first line. sideNum: useless now as I dunno how to calculate side length. angleNum: how many sides are connected to each point. iteCount: used for recursion. plz input 0. r: side length. drawedPoints: plz input {}. color: {r,g,b}. leftMost: input nil.
+local function tesselation(point,angle,sideNum,angleNum,iteCount,r,drawedPoints,color,leftMost)
     color=color or {0.7,0.2,0.5}
     local iteCount=(iteCount or 0)+1
     local points={}
@@ -28,22 +28,14 @@ local function tesselation(point,angle,sideNum,angleNum,iteCount,r,drawedPoints,
     drawedPoints=drawedPoints or {}
     local cic={Shape.getCircle(point.x,point.y,r)}
     -- love.graphics.print(''..cic[1]..', '..cic[2]..' '..cic[3],10,10)
-    local begin=iteCount>1 and 2 or 1
-    for i=begin,angleNum do
-        local alpha=angle+math.pi*2/angleNum*(i-1)
+    local begin=1
+    local en=iteCount>1 and angleNum-2 or angleNum
+    for i=begin,en do
+        local alpha=angle+math.pi*2/angleNum*(i)
         local ret={Shape.rThetaPos(point.x,point.y,r,alpha)}
         local newpoint={x=ret[1],y=ret[2]}
         points[#points+1]=newpoint
         -- SetFont(18)
-        local flag=true
-        local ratio=4.5
-        for k,v in pairs(drawedPoints) do
-            if ((point.x-v[1].x)^2+(point.y-v[1].y)^2+(newpoint.x-v[2].x)^2+(newpoint.y-v[2].y)^2)<ratio*point.y or ((point.x-v[2].x)^2+(point.y-v[2].y)^2+(newpoint.x-v[1].x)^2+(newpoint.y-v[1].y)^2)<ratio*point.y then
-                flag=false
-                break
-            end
-        end
-        if flag then
             table.insert(drawedPoints,{point,newpoint})
             local colorref={love.graphics.getColor()}
             love.graphics.setColor(color[1],color[2],color[3])
@@ -51,9 +43,11 @@ local function tesselation(point,angle,sideNum,angleNum,iteCount,r,drawedPoints,
             love.graphics.setColor(0.35,0.15,0.8)
             PolyLine.drawOne({x=point.x+1,y=point.y+1},{x=newpoint.x+1,y=newpoint.y+1})
             love.graphics.setColor(colorref[1],colorref[2],colorref[3])
-        end
         -- Shape.drawLine(point.x,point.y,newpoint.x,newpoint.y)
         -- love.graphics.print(''..newpoint.x..', '..newpoint.y..' '..alpha..' '..ret[3],10,10+50*i)
+    end
+    if leftMost and iteCount>2 then
+        table.remove(points,1)
     end
     if iteCount==4 then return {},{} end
     local angles={}
@@ -61,11 +55,10 @@ local function tesselation(point,angle,sideNum,angleNum,iteCount,r,drawedPoints,
         local newpoint=points[i]
         local newangle=Shape.to(newpoint.x,newpoint.y,point.x,point.y)
         table.insert(angles,newangle)
-        tesselation(newpoint,newangle,sideNum,angleNum,iteCount,r,drawedPoints,color)
+        tesselation(newpoint,newangle,sideNum,angleNum,iteCount,r,drawedPoints,color,i==1)
     end
     return points,angles
 end
-
 
 local Tesselation=BackgroundPattern:extend()
 function Tesselation:new(args)
