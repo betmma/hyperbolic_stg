7de49287557a373031da1979205c5235c62cc015
betmma <betmma@users.noreply.github.com>
2026-03-10 15:03:19 +0800

    add conf.lua

---
+++ conf.lua
@@ -0,0 +1,4 @@
+function love.conf(t)
+    t.identity = "hyperbolic-domain"
+    t.version = "11.5"
+end
--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
-VERSION="1.0.0.3"
+VERSION="1.0.0.4"
 WINDOW_WIDTH,WINDOW_HEIGHT=love.graphics.getDimensions()
 -- DEV_MODE=true
 if arg[2] == "debug" then
